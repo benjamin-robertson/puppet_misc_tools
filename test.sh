cb4bc2a37c93307320cdcdab93cb2aa4c1678885
ben <benrobertson9876@gmail.com>
2024-05-02 12:45:11 +1000

    more commits

--- test.sh
+++ test.sh
@@ -11,13 +11,57 @@ do
         loop=true
         count=$doge
     fi
+    re='^/.*'
+    if [[ $doge =~ $re ]]
+    then
+        echo "I am file path $doge"
+        file=$doge
+        file_true=true
+    fi
+done
+
+for doge in {1..2}
+do
+    echo "doge is $doge"
 done
 
 if [[ $loop -eq true ]]
 then
-    for doge in {1..$count}
+    for (( c=0; c<$count; c++ ))
     do
-        echo $doge
+        echo $c
+    done
+    iter=0
+    while [[ $iter -lt $count ]]
+    do
+        echo "poodle"
+        iter=$iter+1
+    done
+    for doge in $(eval echo "{0..$count}")
+    do
+        echo "evel $doge"
     done
 fi
 
+# arrays
+louie_array=( brown cool awesome )
+echo ${louie_array[2]}
+
+# or
+declare -a dog
+dog['colour']="brown"
+dog['breed']='spoodle'
+dog['age']=8
+
+echo ${dog['age']}
+
+# check if file exists
+if [[ $file_true -eq true ]]
+then
+    ls $file 1> /dev/null 2> /dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "File $file exists"
+    else
+        echo "File $file does not exist"
+    fi
+fi
